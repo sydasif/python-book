9c61ce3128db1909a6e1ab38abc6fe6f3a478ce9
syed asif <sydasif78@gmail.com>
2022-11-19 11:06:21 +0500

    add logo and title

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,6 +53,9 @@ exclude_patterns = []
 html_theme = "sphinx_book_theme"
 ...
 
+html_logo = "../../data-type.PNG"
+html_title = "Python for Network Automation"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
